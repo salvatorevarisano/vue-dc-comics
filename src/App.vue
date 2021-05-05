e098329b0caaf318fa6fbe26b3006c053553733a
salvatorevarisano <salvator3varisano@gmail.com>
2021-05-05 17:19:21 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  // GENERAL
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+
+  li {
+    list-style: none;
+    a {
+      text-decoration: none;
+      color: currentColor;
+    }
+  }
+
+
+  // UTILITIES
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+      <div class="container">
+          <a href="#">
+              <img class="brand" src="../assets/img/dc-logo.png" alt="logo DC">
+          </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                    <li>
+                        <a href="#">shop</a>
+                    </li>
+                </ul>
+            </nav>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        font-weight: 600;
+        text-transform: uppercase;
+    }
+    .container {
+        display: flex;
+        height: 120px;
+        justify-content: space-between;
+        align-items: center;
+        .brand {
+            height: 80px;
+        }
+        li {
+            display:inline-flex;
+            &:not(:last-of-type) {
+                margin-right: 1.8rem;
+            }
+        }
+    }
+</style>
